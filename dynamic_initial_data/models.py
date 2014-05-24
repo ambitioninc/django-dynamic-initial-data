1117948788b86a9bc25ebeb5a490d326f9466a9f
Wes Kendall <wes.kendall@tryambition.com>
2014-05-24 16:28:47 -0400

    django dynamic initial data handles proxy models properly

--- dynamic_initial_data/models.py
+++ dynamic_initial_data/models.py
@@ -12,7 +12,7 @@ class RegisteredForDeletionReceipt(models.Model):
     # The model object that was registered
     model_obj_type = models.ForeignKey(ContentType)
     model_obj_id = models.PositiveIntegerField()
-    model_obj = generic.GenericForeignKey('model_obj_type', 'model_obj_id')
+    model_obj = generic.GenericForeignKey('model_obj_type', 'model_obj_id', for_concrete_model=False)
 
     # The time at which it was registered for deletion
     register_time = models.DateTimeField()
--- dynamic_initial_data/tests/models.py
+++ dynamic_initial_data/tests/models.py
@@ -6,3 +6,11 @@ class Account(models.Model):
     A test account model.
     """
     name = models.CharField(max_length=64)
+
+
+class ProxyAccount(Account):
+    """
+    A model for testing proxy models.
+    """
+    class Meta:
+        proxy = True
