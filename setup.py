b8453963e9f5b3255465b60afbeae01f0502dc2c
Jared Lewis <jared.lewis@ambition.com>
2016-11-28 15:18:13 -0500

    Add python 3.5 support drop django 1.7

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
@@ -38,8 +39,8 @@ setup(
     ],
     license='MIT',
     install_requires=[
-        'django>=1.7',
-        'django-manager-utils>=0.9.1',
+        'django>=1.8',
+        'django-manager-utils>=0.12.0',
     ],
     tests_require=[
         'psycopg2',
