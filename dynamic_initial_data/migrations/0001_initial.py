eeb8310014ea7c3b30e8eb5d1579f598e3b070f5
Wes Kendall <wes.kendall@cloverhealth.com>
2017-12-05 16:15:23 -0800

    add on_delete to migration

--- dynamic_initial_data/migrations/0001_initial.py
+++ dynamic_initial_data/migrations/0001_initial.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
                 ('model_obj_id', models.PositiveIntegerField()),
                 ('register_time', models.DateTimeField()),
-                ('model_obj_type', models.ForeignKey(to='contenttypes.ContentType')),
+                ('model_obj_type', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='contenttypes.ContentType')),
             ],
             options={
             },
