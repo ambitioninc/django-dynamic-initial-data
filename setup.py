ff5e6838d3e2052fc9f2547714339f7d42e48709
Wes Okes <wes.okes@gmail.com>
2019-07-11 14:47:23 -0400

    requirements

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     license='MIT',
     install_requires=[
         'Django>=2.0',
-        'django-manager-utils>=1.1.1',
+        'django-manager-utils>=1.4.0',
     ],
     tests_require=[
         'psycopg2',
